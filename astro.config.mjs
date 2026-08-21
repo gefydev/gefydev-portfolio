7639dcbc8ba4adf860bea23aee3d75930ce03e38
GenaDeev <genaroelgenio@hotmail.com>
2026-08-20 22:28:09 -0300

    fix: added vercel server adapter to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://gefy.dev',
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
